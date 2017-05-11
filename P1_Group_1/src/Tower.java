163b816a33d07bf54b298169d1d2bae58fa04238
lumisphere902 <tpjunk0@gmail.com>
2017-05-11 08:50:40 -0700

    Updated tower

--- P1_Group_1/src/Tower.java
+++ P1_Group_1/src/Tower.java
@@ -1,20 +1,16 @@
 
 public abstract class Tower extends Actor {
 	private int id;
-	private int type;
 	private int x;
 	private int y;
-	public Tower(int id, int type, int x, int y) {
+	public Tower(int id, int x, int y) {
 		super();
 		this.id = id;
-		this.type = type;
 		this.x = x;
 		this.y = y;
 	}
 	public abstract void attack();
-	public void upgrade() {
-		
-	}
+	public abstract void upgrade();
 	public void sell() {
 		
 	}
