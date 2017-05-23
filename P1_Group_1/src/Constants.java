674859f96595c6536495c148c4fc8f9e0c96cb02
saagarsanghavi <saagarsanghavi@192.168.0.10>
2017-05-23 11:43:23 -0700

    Changed some constants so that its consistent with hardcoded values

---
+++ P1_Group_1/src/Constants.java
@@ -0,0 +1,37 @@
+	
+import javafx.scene.image.Image;
+
+public final class Constants {
+	public static final int[][] waves = {
+//	 minBasic	maxBasic	timePerBasic
+	{3,			5,			3000}
+	};
+	
+	public static final TowerData[] towerTypes = {
+//		
+	new TowerData(50,"basicTower.png")
+	};
+}
+class TowerData {
+	private int cost;
+	private Image image;
+	public TowerData(int cost, String image) {
+		super();
+		this.cost = cost;
+		this.image = new Image("file:" + image, 50, 50, false, false);
+		System.out.println(this.image.getWidth());
+	}
+	public int getCost() {
+		return cost;
+	}
+	public void setCost(int cost) {
+		this.cost = cost;
+	}
+	public Image getImage() {
+		return image;
+	}
+	public void setImage(Image image) {
+		this.image = image;
+	}
+	
+}
